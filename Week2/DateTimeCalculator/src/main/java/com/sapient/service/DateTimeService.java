d2e2b6b51e56369733fb372814dd7929509ec2a6
Abhas KG <abhasgorain@gmail.com>
2020-08-24 12:19:25 +0530

    DateTime Initialised

---
+++ Week2/DateTimeCalculator/src/main/java/com/sapient/service/DateTimeService.java
@@ -0,0 +1,34 @@
+package com.sapient.service;
+
+import java.util.Date;
+
+public class DateTimeService {
+
+
+	public Date addToDate(Date date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Date subtractFromDate(Date date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer differenceBetweenDates(Date d1, Date d2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getWeekNumber(Date date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String dayOfWeek(Date date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
---
+++ Week2/DateTimeCalculator/src/main/java/com/sapient/util/InputReader.java
@@ -0,0 +1,9 @@
+package com.sapient.util;
+
+import java.util.Scanner;
+
+public class InputReader {
+
+	public static Scanner read = new Scanner(System.in);
+	
+}
